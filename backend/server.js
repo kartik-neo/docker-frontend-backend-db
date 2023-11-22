362110a244e215bbb1a446ef16779de948d122e3
kartik-neo <143995170+kartik-neo@users.noreply.github.com>
2023-11-22 17:23:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://10.8.7.221:27017/todos", {
+  await mongoose.connect("mongodb://root:password@10.8.7.221:27017/todos", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
