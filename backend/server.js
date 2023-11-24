35d65ac6da90eaa465f40acc41dab18c0a2ed7ef
kartik-neo <143995170+kartik-neo@users.noreply.github.com>
2023-11-24 11:34:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,25 @@ const mongoose = require("mongoose");
 const port = 3001;
 const routes = require("./routes");
 
+const username = "adminuser";
+const password = "password123";
+
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://mongo:mongo@10.8.10.249:27017/todos", {
+  await mongoose.connect("mongodb://10.8.10.249:27017/todos", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    auth: { user: mongo, password: mongo },
   });
+
   const app = express();
   app.use(cors());
   app.use(express.json());
   app.use("/api", routes);
-  app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the home page!');
-});
 
   app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
   });
 }
+
